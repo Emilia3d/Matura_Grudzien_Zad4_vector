5c1335fd0f9021d802f58797e5b02e708554a520
Emilia <emilia3d.pulawy@gmail.com>
2025-10-27 08:20:17 +0100

    4.3

--- Matura_Grudzien_Zad4_vector/Matura_Grudzien_Zad4_vector.cpp
+++ Matura_Grudzien_Zad4_vector/Matura_Grudzien_Zad4_vector.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
-#include <iostream>
+#include <vector>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -41,5 +42,60 @@ int main() {
     for (auto wys_szer : dane) {
         cout << wys_szer.first << " " << wys_szer.second << endl;
     }
+
+    //Zad. 4.1
+    cout << "4.1" << endl;
+    int min_pole = INT_MAX, max_pole = INT_MIN;
+    for (size_t i = 0; i < wysokosc.size(); ++i) {
+        int pole = wysokosc[i] * szerokosc[i];
+        min_pole = min(min_pole, pole);
+        max_pole = max(max_pole, pole);
+    }
+    cout << min_pole << endl;
+    cout << max_pole << endl;
+    int max_dlugosc = 1, dlugosc = 1;
+    int ostatnia_wysokosc = wysokosc[0], ostatnia_szerokosc = szerokosc[0];
+
+    //Zad. 4.2
+    cout << "4.2" << endl;
+    for (int i = 1; i < wysokosc.size(); ++i) {
+        if (wysokosc[i] <= ostatnia_wysokosc && szerokosc[i] <= ostatnia_szerokosc) {
+            dlugosc++;
+        }
+        else {
+            dlugosc = 1; // Resetujemy ciąg
+        }
+        max_dlugosc = max(max_dlugosc, dlugosc);
+
+        ostatnia_wysokosc = wysokosc[i];
+        ostatnia_szerokosc = szerokosc[i];
+    }
+
+    cout << max_dlugosc << endl;
+
+    //Zad. 4.3
+    cout << "4.3" << endl;
+
+    vector<vector<int>> szerokosci_po_wysokosciach(40001); // 40001, ponieważ maksymalna wysokość to 40 000
+
+    // Grupujemy prostokąty po wysokościach
+    for (int i = 0; i < wysokosc.size(); ++i) {
+        szerokosci_po_wysokosciach[wysokosc[i]].push_back(szerokosc[i]);
+    }
+
+    int suma2s = 0;
+
+    // Przechodzimy po wszystkich wysokościach
+    for (int i = 0; i <= 40000; ++i) {
+        if (szerokosci_po_wysokosciach[i].size() >= 2) {
+            sort(szerokosci_po_wysokosciach[i].rbegin(), szerokosci_po_wysokosciach[i].rend()); // Sortujemy szerokości malejąco
+            int suma = szerokosci_po_wysokosciach[i][0] + szerokosci_po_wysokosciach[i][1]; // Suma szerokości dwóch pierwszych prostokątów
+            suma2s = max(suma2s, suma);
+        }
+    }
+
+    cout << "Największa szerokość przy sklejaniu 2 prostokątów: " << suma2s << endl;
+
+
     return 0;
 }
